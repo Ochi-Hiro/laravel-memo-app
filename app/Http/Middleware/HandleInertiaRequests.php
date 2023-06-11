e5563c5392bbeee9e69c156e37c4431f2ebb68fe
shiro-kuma <120717898+shiro-kuma92@users.noreply.github.com>
2023-06-12 08:18:37 +0900

    flash message

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,6 +39,10 @@ class HandleInertiaRequests extends Middleware
                     'location' => $request->url(),
                 ]);
             },
+            'flash' => [
+                'message' => fn() => $request -> session() -> get('message')
+                // Fn()挟むことで部分リロード。セッションからmessageの取得を行う。
+            ],
         ]);
     }
 }
--- resources/js/Pages/InertiaDemo/index.jsx
+++ resources/js/Pages/InertiaDemo/index.jsx
@@ -1,11 +1,18 @@
 import { Head } from '@inertiajs/react';
 import { Link } from '@inertiajs/react';
+import { usePage } from '@inertiajs/react'
 
 export default function Index() {
+  const { flash } = usePage().props
+
   return (
     <>
       <Head title="Index" />
       
+      {flash.message && (
+        <div className='text-rose-500 ml-3'>{flash.message}</div>
+      )}
+
       <div className='p-6 text-gray-900'>
         InertiaTest from InertiaTestController
       </div>
