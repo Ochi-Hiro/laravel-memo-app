d163bdacac88109324e00e16984fae63a8d19bca
shiro-kuma <120717898+shiro-kuma92@users.noreply.github.com>
2023-06-16 08:20:18 +0900

    make index indention

--- resources/js/Pages/InertiaDemo/Create.jsx
+++ resources/js/Pages/InertiaDemo/Create.jsx
@@ -1,7 +1,8 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { useState } from 'react'
 import { router, usePage } from '@inertiajs/react'
 
-export default function Edit() {
+export default function Edit({auth}) {
 
   const { errors } = usePage().props
 
@@ -26,37 +27,41 @@ export default function Edit() {
 
   return (
     <>
-      <div className='text-2xl text-gray-900 m-auto mt-12 text-center'>InertiaTest CreatePage</div>
-      <form onSubmit={ handleSubmit }>
-        <label htmlFor="title" className="border-gray-300 ml-3">title:</label>
-        <input 
-          id="title"
-          name='title'
-          type="text"
-          value={ values.title }
-          onChange={ handleChange }
-          className="border-gray-300 m-3"
-        />
-        {errors.title && <div className="text-rose-500 ml-3">{errors.title}</div>}
-        <br/>
-        <label htmlFor="content" className="border-gray-300 ml-3">content:</label>
-        <input 
-          id="content"
-          name='content'
-          type="text"
-          value={ values.content }
-          onChange={ handleChange }
-          className="border-gray-300 m-3"
-        />
-        {errors.content && <div className="text-rose-500 ml-3">{errors.content}</div>}
-        <br />
-        <button 
-          type="submit"
-          className="p-2 m-3 bg-gray-800 border border-transparent rounded-md font-semibold text-white"
-        >
-          Submit
-        </button>
-    </form>
+      <AuthenticatedLayout
+        user={auth.user}
+      >
+        <div className='text-2xl text-gray-900 m-auto mt-12 text-center'>InertiaTest CreatePage</div>
+        <form onSubmit={ handleSubmit }>
+          <label htmlFor="title" className="border-gray-300 ml-3">title:</label>
+          <input 
+            id="title"
+            name='title'
+            type="text"
+            value={ values.title }
+            onChange={ handleChange }
+            className="border-gray-300 m-3"
+          />
+          {errors.title && <div className="text-rose-500 ml-3">{errors.title}</div>}
+          <br/>
+          <label htmlFor="content" className="border-gray-300 ml-3">content:</label>
+          <input 
+            id="content"
+            name='content'
+            type="text"
+            value={ values.content }
+            onChange={ handleChange }
+            className="border-gray-300 m-3"
+          />
+          {errors.content && <div className="text-rose-500 ml-3">{errors.content}</div>}
+          <br />
+          <button 
+            type="submit"
+            className="p-2 m-3 bg-gray-800 border border-transparent rounded-md font-semibold text-white"
+          >
+            Submit
+          </button>
+      </form>
+    </AuthenticatedLayout>
     </>
   )
 }
